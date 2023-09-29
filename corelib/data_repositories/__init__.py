93e92ce9a2315e44b89e753720cec4f041a49e3b
HeberTU <heber.trujillo@glovoapp.com>
2023-09-29 20:13:22 +0200

    feat: Create data repository abstraction.

---
+++ corelib/data_repositories/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Data Repository library.
+
+This library is used to encapsulate all the data layer related modules.
+
+Created on: 29/9/23
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
