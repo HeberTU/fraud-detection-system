0664b4c1d0f8790eca03e04542962d3bd49d102b
HeberTU <heber.trujillo@glovoapp.com>
2023-09-30 06:39:10 +0200

    feat: create data repository parameters.

---
+++ corelib/data_repositories/data_repository_params.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Data repository parameters.
+
+Created on: 30/9/23
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
+from dataclasses import dataclass
+
+
+@dataclass
+class SyntheticParams:
+    """Synthetic data repository parameters."""
+
+    customer_uniform_lower_bound: int
+    customer_uniform_upper_bound: int
+    amount_uniform_lower_bound: int
+    amount_uniform_upper_bound: int
+    trans_uniform_lower_bound: int
+    trans_uniform_upper_bound: int
