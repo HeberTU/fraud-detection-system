711db7078a3681dd94bac2e54902e3cbc87f84e6
HeberTU <heber.trujillo@glovoapp.com>
2023-09-30 06:52:13 +0200

    feat: Add test for the generate customer profiles routine.

--- corelib/domain/__init__.py
+++ corelib/domain/__init__.py
@@ -5,8 +5,10 @@ Created on: 29/9/23
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
+from corelib.domain.data_simulator import generate_customer_profiles_list
 from corelib.domain.models import CustomerProfile
 
 __all__ = [
     "CustomerProfile",
+    "generate_customer_profiles_list",
 ]
