104bcab8856579549250e24b98c919a6a0d21ef6
HeberTU <heber.trujillo@glovoapp.com>
2023-10-03 11:43:55 +0200

    fix: output probs from decision get scores method.

--- corelib/ml/algorithms/decision_tree.py
+++ corelib/ml/algorithms/decision_tree.py
@@ -5,6 +5,7 @@ Created on: 3/10/23
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
+import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
 from sklearn.tree import DecisionTreeClassifier
@@ -13,29 +14,31 @@ from sklearn.tree import DecisionTreeClassifier
 class DecisionTree(DecisionTreeClassifier):
     """Decision tree classifier wrapper."""
 
-    def get_predictions(self, data: pd.DataFrame) -> NDArray:
+    def get_predictions(self, features: pd.DataFrame) -> NDArray:
         """Wraps the predict method.
 
         Args:
-            data: pd.DataFrame
+            features: pd.DataFrame
                 Data frame that will be used to generate predictions.
 
         Returns:
             NDArray:
                 model predictions
         """
-        return self.predict(X=data)
+        return self.predict(X=features)
 
-    def get_scores(self, data: pd.DataFrame) -> NDArray:
+    def get_scores(self, features: pd.DataFrame) -> NDArray:
         """Wraps the predict log probs method.
 
         Args:
-            data: pd.DataFrame
+            features: pd.DataFrame
                 Data frame that will be used to generate predictions.
 
         Returns:
             NDArray:
                 model predictions
         """
-        log_probs = self.predict_log_proba(X=data)
+        log_probs = self.predict_log_proba(X=features)
+        log_probs = log_probs[:, 1]
+        log_probs = np.exp(log_probs)
         return log_probs
