e817df0d0abd8251c1a85b489880d0674e93ceee
HeberTU <heber.trujillo@glovoapp.com>
2023-10-03 18:19:27 +0200

    feat: Add lightgbm algorithm.

--- corelib/ml/algorithms/algorithm_params.py
+++ corelib/ml/algorithms/algorithm_params.py
@@ -10,8 +10,28 @@ from dataclasses import dataclass
 
 @dataclass
 class DecisionTreeParams:
-    """Time evaluator parameters."""
+    """Decision tree parameters."""
 
     criterion: str = "gini"
     max_depth: int = 2
     random_state: int = 0
+
+
+@dataclass
+class LightGBMParams:
+    """Light GBM parameters."""
+
+    objective: str = "xentropy"
+    num_iterations: int = 100
+    max_depth: int = 3
+    num_leaves: int = 30
+    learning_rate: float = 0.05
+    bagging_fraction: float = 1
+    feature_fraction: float = 1
+    min_gain_to_split: float = 1
+    min_data_in_leaf: float = 1
+    lambda_l1: float = 0
+    lambda_l2: float = 1
+    num_threads: int = -1
+    verbose: int = -1
+    threshold: float = 0.5
