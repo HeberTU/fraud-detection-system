28404478256285d766ca8bbf13652ff99dbfd5bb
HeberTU <heber.trujillo@glovoapp.com>
2023-10-04 07:26:40 +0200

    feat: adjust card precision by removing the previously detected cards.

--- corelib/domain/kpis.py
+++ corelib/domain/kpis.py
@@ -5,7 +5,11 @@ Created on: 3/10/23
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
-from typing import Tuple
+from typing import (
+    Dict,
+    List,
+    Union,
+)
 
 import numpy as np
 import pandas as pd
@@ -13,7 +17,7 @@ import pandas as pd
 
 def card_precision_top_k_day(
     df_day: pd.DataFrame, top_k: int = 100
-) -> Tuple[float, float]:
+) -> Dict[str, Union[float, List[int]]]:
     """Calculate the card precision top-k per day.
 
     Args:
@@ -23,9 +27,10 @@ def card_precision_top_k_day(
             Number of transactions that will be checked.
 
     Returns:
-        Tuple[float, float]:
+        Dict[str, Union[float, List[int]]]:
             Card precision top-k.
-            perfect Card precision top-k.
+            Perfect Card precision top-k.
+            Detected compromised cards list.
     """
     # Order transactions by decreasing probabilities of frauds
     df_day = (
@@ -37,13 +42,23 @@ def card_precision_top_k_day(
     # Get the top k most suspicious transactions
     df_day_top_k = df_day.head(top_k)
 
+    results = {
+        "card_precision_top_k": df_day_top_k.tx_fraud.mean(),
+        "perfect_card_precision_top_k": df_day.tx_fraud.sum() / 100,
+        "detected_compromised_cards_list": df_day_top_k[
+            df_day_top_k.tx_fraud == 1
+        ].customer_id.values.tolist(),
+    }
+
     # % of real frauds out of the k most suspicious
-    return df_day_top_k.tx_fraud.mean(), df_day.tx_fraud.sum() / 100
+    return results
 
 
 def card_precision_top_k(
-    test_data: pd.DataFrame, top_k: int = 100
-) -> Tuple[float, float]:
+    test_data: pd.DataFrame,
+    top_k: int = 100,
+    remove_detected_compromised_cards: bool = True,
+) -> Dict[str, float]:
     """Calculate the average card precision top-k metric per day.
 
     Args:
@@ -51,6 +66,8 @@ def card_precision_top_k(
             Testing data that will be used to calculate the metric.
         top_k: int
             Number of transactions that will be checked.
+        remove_detected_compromised_cards: bool
+            If True, previously compromised cards will be removed.
 
     Returns:
         Tuple[float, float]:
@@ -60,6 +77,8 @@ def card_precision_top_k(
     start_date = test_data.tx_datetime.min().floor(freq="D")
     end_date = test_data.tx_datetime.max().ceil(freq="D")
     num_days = (end_date - start_date).days
+
+    list_detected_compromised_cards = []
 
     precision_top_k_per_day_list = []
     perfect_precision_top_k_per_day_list = []
@@ -76,14 +95,22 @@ def card_precision_top_k(
                 test_data.tx_datetime
                 > start_date + pd.Timedelta(value=i, unit="days")
             )
+            & ~(test_data.customer_id.isin(list_detected_compromised_cards))
         ]
 
-        precision_top_k_value, max_performance = card_precision_top_k_day(
-            df_day=df_day, top_k=top_k
+        results = card_precision_top_k_day(df_day=df_day, top_k=top_k)
+
+        precision_top_k_per_day_list.append(
+            results.get("card_precision_top_k")
+        )
+        perfect_precision_top_k_per_day_list.append(
+            results.get("perfect_card_precision_top_k")
         )
 
-        precision_top_k_per_day_list.append(precision_top_k_value)
-        perfect_precision_top_k_per_day_list.append(max_performance)
+        if remove_detected_compromised_cards:
+            list_detected_compromised_cards.extend(
+                results.get("detected_compromised_cards_list")
+            )
 
     # Compute the mean
     mean_precision_top_k = np.round(
