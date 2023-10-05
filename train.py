3f25a8b423e259a3e75c79b44ace49c4d23b63cb
HeberTU <heber.trujillo@glovoapp.com>
2023-10-05 15:42:54 +0200

    ref: create implementations for estimators.

--- train.py
+++ train.py
@@ -16,6 +16,7 @@ from corelib import (
 def main(do_hpo: bool):
     """Execute main script."""
     estimator = ml.EstimatorFactory.create(
+        estimator_type=ml.EstimatorType.ML_ESTIMATOR,
         data_repository_type=data_repositories.DataRepositoryType.SYNTHETIC,
         evaluator_type=ml.EvaluatorType.TIME_EVALUATOR,
         algorithm_type=ml.AlgorithmType.LIGHT_GBM,
