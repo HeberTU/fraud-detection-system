9dd942a2d00ad8c7b17f58de2ab21a4467b431a6
HeberTU <heber.trujillo@glovoapp.com>
2023-10-09 07:26:02 +0200

    ref: create a routes module to implement the SRP.

--- corelib/entrypoints/api.py
+++ corelib/entrypoints/api.py
@@ -5,52 +5,13 @@ Created on: 4/10/23
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
-from fastapi import (
-    Depends,
-    FastAPI,
-)
+from fastapi import FastAPI
 
-from corelib.config import settings
-from corelib.entrypoints.assets import Assets
-from corelib.ml.estimators.estimator import Estimator
-from corelib.services.contracts import (
-    PredictionRequest,
-    PredictionResponse,
-)
-from corelib.services.prediction_service import PredictionService
-
-estimator = Assets(settings.ENV)()
-
-app = FastAPI()
+from corelib.entrypoints.routes import get_router
 
 
-@app.post(
-    "/model/v0/prediction/{transaction_id}", response_model=PredictionResponse
-)
-def predict(
-    transaction_id: str,
-    request: PredictionRequest,
-    estimator: Estimator = Depends(lambda: estimator),
-) -> PredictionResponse:
-    """Prediction endpoint.
-
-    Args:
-        transaction_id: str
-            Unique ID for the transaction.
-        request: ModifiedPredictionRequest
-            Input features.
-        estimator: Estimator
-            Estimator class.
-
-    Returns:
-        ModifiedPredictionResponse:
-            Decision to block the transaction or not.
-    """
-    service = PredictionService(estimator)
-    prediction = service.make_prediction(
-        prediction_request=request, transaction_id=int(transaction_id)
-    )
-
-    return PredictionResponse(
-        transaction_id=transaction_id, transaction_to_block=prediction
-    )
+def get_api() -> FastAPI:
+    """Get API."""
+    app = FastAPI
+    app.include_router(get_router())
+    return app
